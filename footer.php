6bdcc6d3688b08ebc481c9e2a0533aaff9a3420e
ksaurabh2315 <90023381+ksaurabh2315@users.noreply.github.com>
2022-05-16 20:51:04 +0530

    Add files via upload

--- footer.php
+++ footer.php
@@ -1,10 +1,28 @@
-<!-- Footer Section Starts -->
-<div class="footer">
-            <div class="wrapper">
-                <p class="text-center">2022 All rights reserved, wow foods. Developed By - <a href="#"> 714-B</a></p>
-            </div>
+<!-- social Section Starts Here -->
+<section class="social">
+        <div class="container text-center">
+            <ul>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/50/000000/facebook-new.png"/></a>
+                </li>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/48/000000/instagram-new.png"/></a>
+                </li>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/48/000000/twitter.png"/></a>
+                </li>
+            </ul>
         </div>
-        <!-- Footer Section Ends -->
+    </section>
+    <!-- social Section Ends Here -->
 
-    </body>
+    <!-- footer Section Starts Here -->
+    <section class="footer">
+        <div class="container text-center">
+            <p>All rights reserved wow foods. Designed By <a href="#">714-B</a></p>
+        </div>
+    </section>
+    <!-- footer Section Ends Here -->
+
+</body>
 </html>
