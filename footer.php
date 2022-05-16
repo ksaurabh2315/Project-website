9805887aeefc58fca7931df47bbec8cbc1264ac2
ksaurabh2315 <90023381+ksaurabh2315@users.noreply.github.com>
2022-05-16 20:49:41 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,10 @@
+<!-- Footer Section Starts -->
+<div class="footer">
+            <div class="wrapper">
+                <p class="text-center">2022 All rights reserved, wow foods. Developed By - <a href="#"> 714-B</a></p>
+            </div>
+        </div>
+        <!-- Footer Section Ends -->
+
+    </body>
+</html>
